e42553abbcbe2388dc101970041237669e484940
Osdark <odvelasquez@unac.edu.co>
2018-05-04 09:49:05 -0500

    merge-Quinto,master/config apps

--- SieV1_/Apps/actividad/models.py
+++ SieV1_/Apps/actividad/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Pastor(models.Model):
+    nombre = models.CharField(max_length=120, blank=True, null=True)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Pastor"
+        verbose_name_plural = "Pastores"
