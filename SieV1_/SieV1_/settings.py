0406892094ce21bb547eff3520476522dc28fc03
ElQuinto1998 <37389379+ElQuinto1998@users.noreply.github.com>
2018-05-07 21:49:11 -0500

    confi bd
    Creacion de db mysql

--- SieV1_/SieV1_/settings.py
+++ SieV1_/SieV1_/settings.py
@@ -74,11 +74,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'SieV1_.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'mysql_cymysql',
         'NAME': 'sieData',
         'USER': 'root',
